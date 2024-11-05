983df6b653702abae61cc004b66adc85bd6e9956
Swimnato <66497456+Swimnato@users.noreply.github.com>
2024-11-04 18:04:26 -0700

    started work on client

--- client/src/main/java/Main.java
+++ client/src/main/java/Main.java
@@ -5,16 +5,42 @@ import static chess.ui.EscapeSequences.*;
 
 public class Main {
     public static void main(String[] args) {
-        var piece = new ChessPiece(WHITE, ChessPiece.PieceType.PAWN);
-        var board = new ChessBoard();
-        var game = new ChessGame();
-        System.out.println(ERASE_SCREEN + SET_TEXT_BOLD + SET_TEXT_COLOR_WHITE + SET_BG_COLOR_BLACK + "♕ 240 Chess Client: ");
-        System.out.println(piece);
-        System.out.println(board);
-        board.resetBoard();
-        System.out.println(board.toString(WHITE));
-        System.out.println(board.toString(BLACK));
-        System.out.println(game.toString(WHITE));
-        System.out.println(game.toString(BLACK));
+        System.out.println(ERASE_SCREEN + SET_TEXT_BOLD + SET_TEXT_COLOR_WHITE + SET_BG_COLOR_BLACK + "♕ 240 Chess Client - Type 'Help' to get started");
+
+        boolean running = true;
+        LoginStatus loggedIn = LoginStatus.LOGGED_OUT;
+
+        System.out.print(loggedIn + ">> ");
+        while (running) {
+            try {
+                if (System.in.available() > 0) {
+                    char currentCharacter = ' ';
+                    StringBuilder command = new StringBuilder();
+                    while (currentCharacter != '\n') {
+                        currentCharacter = (char) System.in.read();
+                        command.append(currentCharacter);
+                    }
+                    runCommand(command.toString());
+                    System.out.print(SET_TEXT_COLOR_WHITE + loggedIn + ">> ");
+                }
+            } catch (Exception e) {
+                System.err.println("ERROR - UNCAUGHT EXCEPTION: " + e.getMessage());
+            }
+        }
+    }
+
+    private static void runCommand(String input) {
+        if (input.substring(0, 4).equalsIgnoreCase("Help")) {
+            System.out.println("List Of Available Commands:");
+            System.out.println(SET_TEXT_COLOR_BLUE + "\tRegister " + SET_TEXT_COLOR_GREEN + "<username> <password> <email>" + SET_TEXT_COLOR_LIGHT_GREY + " - To create an account on the server");
+            System.out.println(SET_TEXT_COLOR_BLUE + "\tLogin " + SET_TEXT_COLOR_GREEN + "<username> <password>" + SET_TEXT_COLOR_LIGHT_GREY + " - To play chess");
+            System.out.println(SET_TEXT_COLOR_BLUE + "\tQuit " + SET_TEXT_COLOR_LIGHT_GREY + " - To close the client");
+            System.out.println(SET_TEXT_COLOR_BLUE + "\tHelp " + SET_TEXT_COLOR_LIGHT_GREY + " - Display available commands");
+        }
+    }
+
+    enum LoginStatus {
+        LOGGED_IN,
+        LOGGED_OUT
     }
 }
