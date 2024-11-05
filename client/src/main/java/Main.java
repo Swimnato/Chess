6f3d3401b813ec5ee9fdb495d6a2ce49f546135c
Swimnato <66497456+Swimnato@users.noreply.github.com>
2024-11-05 10:36:21 -0700

    worked on more commands
    added help for more commands

--- client/src/main/java/Main.java
+++ client/src/main/java/Main.java
@@ -4,11 +4,13 @@ import static chess.ChessGame.TeamColor.*;
 import static chess.ui.EscapeSequences.*;
 
 public class Main {
+
+    static LoginStatus loggedIn = LoginStatus.LOGGED_OUT;
+
     public static void main(String[] args) {
         System.out.println(ERASE_SCREEN + SET_TEXT_BOLD + SET_TEXT_COLOR_WHITE + SET_BG_COLOR_BLACK + "♕ 240 Chess Client - Type 'Help' to get started");
 
         boolean running = true;
-        LoginStatus loggedIn = LoginStatus.LOGGED_OUT;
 
         System.out.print(loggedIn + ">> ");
         while (running) {
@@ -34,28 +36,61 @@ public class Main {
     private static boolean runCommand(String input) {
         if (input.toLowerCase().contains("help")) {
             if (removeWhitespace(input).length() == 4) {
-                System.out.println("List Of Available Commands:");
-                System.out.println(SET_TEXT_COLOR_BLUE + "\tRegister " + SET_TEXT_COLOR_GREEN + "<username> <password> <email>" + SET_TEXT_COLOR_LIGHT_GREY + " - To create an account on the server");
-                System.out.println(SET_TEXT_COLOR_BLUE + "\tLogin " + SET_TEXT_COLOR_GREEN + "<username> <password>" + SET_TEXT_COLOR_LIGHT_GREY + " - To play chess");
-                System.out.println(SET_TEXT_COLOR_BLUE + "\tQuit " + SET_TEXT_COLOR_LIGHT_GREY + " - To close the client");
-                System.out.println(SET_TEXT_COLOR_BLUE + "\tHelp " + SET_TEXT_COLOR_LIGHT_GREY + " - Display available commands");
-            } else {
-                input = input.substring(4).toLowerCase();
-                if (input.contains("register")) {
-                    System.out.println(SET_TEXT_COLOR_BLUE + "Register" + SET_TEXT_COLOR_LIGHT_GREY + ": This is a command to create an account on the created server.");
-                    System.out.println("Syntax:");
-                    System.out.println(SET_TEXT_COLOR_BLUE + "\tRegister " + SET_TEXT_COLOR_GREEN + "<username> <password> <email>");
-                    System.out.println("Parameters:");
-                    System.out.println(SET_TEXT_COLOR_GREEN + "<username> " + SET_TEXT_COLOR_LIGHT_GREY + "This is your chosen alias on the server, or what you wish to be called.");
-                    System.out.println(SET_TEXT_COLOR_GREEN + "<password> " + SET_TEXT_COLOR_LIGHT_GREY + "A password, to keep your account safe, something you will remember.");
-                    System.out.println(SET_TEXT_COLOR_GREEN + "<email> " + SET_TEXT_COLOR_LIGHT_GREY + "How you can be reached by the server operator.");
+                if (loggedIn == LoginStatus.LOGGED_OUT) {
+                    System.out.println("List Of Available Commands:");
+                    System.out.println(SET_TEXT_COLOR_BLUE + "\tRegister " + SET_TEXT_COLOR_GREEN + "<username> <password> <email>" + SET_TEXT_COLOR_LIGHT_GREY + " - To create an account on the server");
+                    System.out.println(SET_TEXT_COLOR_BLUE + "\tLogin " + SET_TEXT_COLOR_GREEN + "<username> <password>" + SET_TEXT_COLOR_LIGHT_GREY + " - To play chess");
+                    System.out.println(SET_TEXT_COLOR_BLUE + "\tQuit " + SET_TEXT_COLOR_LIGHT_GREY + " - To close the client");
+                    System.out.println(SET_TEXT_COLOR_BLUE + "\tHelp " + SET_TEXT_COLOR_LIGHT_GREY + " - Display available commands");
                 }
+            } else {
+                printHelpForCommand(input);
             }
         } else if (input.toLowerCase().contains("quit")) {
             System.out.println("Goodbye! Come back soon! :)");
             return false;
+        } else if (input.toLowerCase().contains("register")) {
+
+        } else if (input.toLowerCase().contains("login")) {
+
+        } else {
+
         }
         return true;
+    }
+
+    private static void printHelpForCommand(String input) {
+        input = input.substring(4).toLowerCase();
+        if (loggedIn == LoginStatus.LOGGED_OUT && input.contains("register")) {
+            System.out.println(SET_TEXT_COLOR_BLUE + "Register" + SET_TEXT_COLOR_LIGHT_GREY + ": This is a command to create an account on the created server.");
+            System.out.println("Syntax:");
+            System.out.println(SET_TEXT_COLOR_BLUE + "\tRegister " + SET_TEXT_COLOR_GREEN + "<username> <password> <email>");
+            System.out.println("Parameters:");
+            System.out.println(SET_TEXT_COLOR_GREEN + "\t<username> " + SET_TEXT_COLOR_LIGHT_GREY + "This is your chosen alias on the server, or what you wish to be called.");
+            System.out.println(SET_TEXT_COLOR_GREEN + "\t<password> " + SET_TEXT_COLOR_LIGHT_GREY + "A password, to keep your account safe, something you will remember.");
+            System.out.println(SET_TEXT_COLOR_GREEN + "\t<email> " + SET_TEXT_COLOR_LIGHT_GREY + "How you can be reached by the server operator.");
+        } else if (loggedIn == LoginStatus.LOGGED_OUT && input.contains("login")) {
+            System.out.println(SET_TEXT_COLOR_BLUE + "Login" + SET_TEXT_COLOR_LIGHT_GREY + ": This is a command to login into a server. If you need to create an account, use " + SET_TEXT_COLOR_BLUE + "\tRegister ");
+            System.out.println("Syntax:");
+            System.out.println(SET_TEXT_COLOR_BLUE + "\tLogin " + SET_TEXT_COLOR_GREEN + "<username> <password>");
+            System.out.println("Parameters:");
+            System.out.println(SET_TEXT_COLOR_GREEN + "\t<username> " + SET_TEXT_COLOR_LIGHT_GREY + "Your Username on the server.");
+            System.out.println(SET_TEXT_COLOR_GREEN + "\t<password> " + SET_TEXT_COLOR_LIGHT_GREY + "Your Password.");
+        } else if (input.contains("quit")) {
+            System.out.println(SET_TEXT_COLOR_BLUE + "Quit" + SET_TEXT_COLOR_LIGHT_GREY + " Stops the client.");
+            System.out.println("Syntax:");
+            System.out.println(SET_TEXT_COLOR_BLUE + "\tQuit");
+            System.out.println(SET_TEXT_COLOR_WHITE + "Parameters:");
+            System.out.println(SET_TEXT_COLOR_GREEN + "\tNone");
+        } else if (input.contains("help")) {
+            System.out.println(SET_TEXT_COLOR_BLUE + "Help" + SET_TEXT_COLOR_LIGHT_GREY + " Shows available commands, alternativly can be used to learn more about a given command.");
+            System.out.println("Syntax:");
+            System.out.println(SET_TEXT_COLOR_BLUE + "\tHelp");
+            System.out.println(SET_TEXT_COLOR_WHITE + "Parameters:");
+            System.out.println(SET_TEXT_COLOR_GREEN + "\t<command>" + SET_TEXT_COLOR_LIGHT_GREY + "(Optional) - The desired command that will be explained. If not given, the system will display all available commands.");
+        } else {
+            System.out.println(SET_TEXT_COLOR_RED + "Invalid Syntax of Help! use \"Help Help\" to see proper syntax.");
+        }
     }
 
     private static String removeWhitespace(String input) {
