754dac4c80ef1051796f819616cc454d444e1526
Swimnato <66497456+Swimnato@users.noreply.github.com>
2024-11-05 17:48:17 -0700

    Simplified code

--- client/src/main/java/Main.java
+++ client/src/main/java/Main.java
@@ -35,41 +35,53 @@ public class Main {
     }
 
     private static boolean runCommand(String input) {
-        CommandParser parser = new CommandParser(input);
-        if (parser.isCommand("Help")) {
-            if (parser.numOfParameters() == 0) {
-                if (loggedIn == LoginStatus.LOGGED_OUT) {
-                    System.out.println("List Of Available Commands:");
-                    System.out.println(SET_TEXT_COLOR_BLUE + "\tRegister " + SET_TEXT_COLOR_GREEN + "<username> <password> <email>" + SET_TEXT_COLOR_LIGHT_GREY + " - To create an account on the server");
-                    System.out.println(SET_TEXT_COLOR_BLUE + "\tLogin " + SET_TEXT_COLOR_GREEN + "<username> <password>" + SET_TEXT_COLOR_LIGHT_GREY + " - To play chess");
-                    System.out.println(SET_TEXT_COLOR_BLUE + "\tQuit " + SET_TEXT_COLOR_LIGHT_GREY + " - To close the client");
-                    System.out.println(SET_TEXT_COLOR_BLUE + "\tHelp " + SET_TEXT_COLOR_LIGHT_GREY + " - Display available commands");
+        try {
+            CommandParser parser = new CommandParser(input);
+            if (parser.isCommand("Help")) {
+                if (parser.numOfParameters() == 0) {
+                    if (loggedIn == LoginStatus.LOGGED_OUT) {
+                        System.out.println("List Of Available Commands:");
+                        System.out.println(SET_TEXT_COLOR_BLUE + "\tRegister " + SET_TEXT_COLOR_GREEN + "<username> <password> <email>" + SET_TEXT_COLOR_LIGHT_GREY + " - To create an account on the server");
+                        System.out.println(SET_TEXT_COLOR_BLUE + "\tLogin " + SET_TEXT_COLOR_GREEN + "<username> <password>" + SET_TEXT_COLOR_LIGHT_GREY + " - To play chess");
+                        System.out.println(SET_TEXT_COLOR_BLUE + "\tQuit " + SET_TEXT_COLOR_LIGHT_GREY + " - To close the client");
+                        System.out.println(SET_TEXT_COLOR_BLUE + "\tHelp " + SET_TEXT_COLOR_LIGHT_GREY + " - Display available commands");
+                    }
+                } else {
+                    printHelpForCommand(parser);
+                }
+            } else if (parser.isCommand("quit")) {
+                if (parser.numOfParameters() == 0) {
+                    System.out.println("Goodbye! Come back soon! :)");
+                    return false;
+                } else {
+                    throw new InvalidSyntaxException("Quit");
+                }
+            } else if (parser.isCommand("register")) {
+                if (parser.numOfParameters() == 3 && loggedIn == LoginStatus.LOGGED_OUT) {
+
+                } else {
+                    throw new InvalidSyntaxException("Register");
+                }
+            } else if (parser.isCommand("login")) {
+                if (parser.numOfParameters() == 2 && loggedIn == LoginStatus.LOGGED_OUT) {
+
+                } else {
+                    throw new InvalidSyntaxException("Login");
                 }
             } else {
-                printHelpForCommand(parser);
+                System.out.println(SET_TEXT_COLOR_RED + "Unrecognized command! Use \"Help\" to find a list of available commands!");
             }
-        } else if (parser.isCommand("quit")) {
-            if (parser.numOfParameters() == 0) {
-                System.out.println("Goodbye! Come back soon! :)");
-                return false;
-            } else {
-                System.out.println(SET_TEXT_COLOR_RED + "Invalid Syntax of Quit! use \"Help Quit\" to see proper syntax.");
-            }
-        } else if (parser.isCommand("register")) {
-
-        } else if (parser.isCommand("login")) {
-
-        } else {
-
+        } catch (InvalidSyntaxException e) {
+            System.out.println(SET_TEXT_COLOR_RED + "Invalid Syntax of " + e.getMessage() + "! Use \"Help " + e.getMessage() + "\" to see proper syntax.");
         }
         return true;
     }
 
-    private static void printHelpForCommand(CommandParser input) {
+    private static void printHelpForCommand(CommandParser input) throws InvalidSyntaxException {
         if (input.numOfParameters() > 1) {
-            System.out.println(SET_TEXT_COLOR_RED + "Invalid Syntax of Help! use \"Help Help\" to see proper syntax.");
+            throw new InvalidSyntaxException("Help");
         } else if (loggedIn == LoginStatus.LOGGED_OUT && input.isParameterEqual(0, "Register")) {
-            System.out.println(SET_TEXT_COLOR_BLUE + "Register" + SET_TEXT_COLOR_LIGHT_GREY + ": This is a command to create an account on the created server.");
+            System.out.println(SET_TEXT_COLOR_BLUE + "Register" + SET_TEXT_COLOR_LIGHT_GREY + ": This is a command to create an account on the created server. This can only be used when logged out.");
             System.out.println("Syntax:");
             System.out.println(SET_TEXT_COLOR_BLUE + "\tRegister " + SET_TEXT_COLOR_GREEN + "<username> <password> <email>");
             System.out.println("Parameters:");
@@ -77,7 +89,7 @@ public class Main {
             System.out.println(SET_TEXT_COLOR_GREEN + "\t<password> " + SET_TEXT_COLOR_LIGHT_GREY + "A password, to keep your account safe, something you will remember.");
             System.out.println(SET_TEXT_COLOR_GREEN + "\t<email> " + SET_TEXT_COLOR_LIGHT_GREY + "How you can be reached by the server operator.");
         } else if (loggedIn == LoginStatus.LOGGED_OUT && input.isParameterEqual(0, "Login")) {
-            System.out.println(SET_TEXT_COLOR_BLUE + "Login" + SET_TEXT_COLOR_LIGHT_GREY + ": This is a command to login into a server. If you need to create an account, use " + SET_TEXT_COLOR_BLUE + "\tRegister ");
+            System.out.println(SET_TEXT_COLOR_BLUE + "Login" + SET_TEXT_COLOR_LIGHT_GREY + ": This is a command to login into a server. This can only be used when logged out. If you need to create an account, use " + SET_TEXT_COLOR_BLUE + "\tRegister ");
             System.out.println("Syntax:");
             System.out.println(SET_TEXT_COLOR_BLUE + "\tLogin " + SET_TEXT_COLOR_GREEN + "<username> <password>");
             System.out.println("Parameters:");
@@ -96,7 +108,7 @@ public class Main {
             System.out.println(SET_TEXT_COLOR_WHITE + "Parameters:");
             System.out.println(SET_TEXT_COLOR_GREEN + "\t<command>" + SET_TEXT_COLOR_LIGHT_GREY + "(Optional) - The desired command that will be explained. If not given, the system will display all available commands.");
         } else {
-            System.out.println(SET_TEXT_COLOR_RED + "Invalid Syntax of Help! use \"Help Help\" to see proper syntax.");
+            throw new InvalidSyntaxException("Help");
         }
     }
 
